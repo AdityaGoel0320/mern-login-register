ff318523c9b62e39c44efefae3116dfe6bce3213
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-07-24 23:55:15 +0530

    register page set

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default function register() {
-  return (
-    <>
-    <h1>Register page</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Register page</h1>
+
+
+            <div className="box">
+
+                <form action="">
+                    <input type="text" placeholder='Enter name' />
+                    <input type="text" placeholder='Enter emailId' />
+                    <input type="text" placeholder='Enter password' />
+                    <input type="text" placeholder='Enter Repassword' />
+
+                    <button type='submit'>Register</button>
+                    <div>or</div>
+                    <button>Login</button>
+                </form>
+            </div>
+        </>
+    )
 }
